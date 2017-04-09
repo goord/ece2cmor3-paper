ef150a58473ec5f269f890a8fd2db3cb018c584d
gvdoord <g.vandenOord@gmail.com>
2017-04-09 13:18:52 +0200

    Added standard res benchmark to paper

--- speedup.py
+++ speedup.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy
 
 xvals = numpy.array([4,8,12,16])
-yvals = numpy.array([2.92,4.34,5.28,7.69])
+lrtimes = [3817,1066,680,662,480]
+hrtimes = [8867,3033,2044,1680,1154]
+lrspeedup = numpy.array([float(lrtimes[0])/x for x in lrtimes[1:5]])
+hrspeedup = numpy.array([float(hrtimes[0])/x for x in hrtimes[1:5]])
 width = 3.2
 
 fig,ax = plt.subplots()
-bars = ax.bar(xvals - 0.5*width,yvals,width,color='r',edgecolor='r')
+barslr = ax.bar(xvals - 0.5*width,lrspeedup,0.5*width,color='b',edgecolor='b')
+barshr = ax.bar(xvals,hrspeedup,0.5*width,color='r',edgecolor='r')
 
 
 ax.spines['top'].set_visible(False)
@@ -17,4 +22,8 @@ ax.set_xlabel("Nr. of threads")
 ax.set_xticks(xvals)
 ax.set_ylabel("Speedup")
 ax.yaxis.grid(True)
+ax.legend((barslr,barshr),("T255","T511"),bbox_to_anchor=(0.25,1))
+
+matplotlib.rcParams.update({'font.size': 22})
+
 plt.show()
